31384ade05ae0303bbe9fb5bd761e239654ca4d0
myeonghyun-7011 <dhvlftmd7013@naver.com>
2023-12-04 11:53:30 +0900

    세팅 완료

---
+++ test/src/test/person.java
@@ -0,0 +1,38 @@
+package test;
+
+public class person {
+
+	String name;
+	int age;
+	public person() {
+		
+	}
+	
+	public person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "person [name=" + name + ", age=" + age + ", getName()=" + getName() + ", getAge()=" + getAge()
+				+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+				+ "]";
+	}
+	
+	
+	
+}
---
+++ test/src/test/simple.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class simple {
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		List<Integer> list = new ArrayList<>();
+		
+		System.out.println("HELLO world!");
+		System.out.println("HELLO world!");
+		System.out.println("HELLO world!");
+	}
+
+}
